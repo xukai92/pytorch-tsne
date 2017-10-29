11c018d5eb33d490f44ee2532514ab5db9053519
Kai Xu <xukai921110@gmail.com>
2017-10-29 15:59:57 +0000

    add some tests

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 from tsne import *
 
 import torch
+import torch.nn as nn
 
 
 print("[tsne.test] testing starts!")
@@ -20,8 +21,39 @@ print("[tsne.test] done")
 
 print("[tsne.test] testing pairwise...")
 
-xs_pairwise = pairwise(xs)
+t = torch.Tensor([[-1.0, 0.0], [1.0, 0.0], [2.0, 0.0]])
+t_pairwise = pairwise(t)
+assert (t_pairwise == torch.Tensor([[0.0, 2.0, 4.5], [2.0, 0.0, 0.5], [4.5, 0.5, 0.0]])).sum() == 9
 
 print("[tsne.test] done")
+
+print("[tsne.test] testing pairwise2gauss...")
+
+Ps = pairwise2gauss(t_pairwise)
+
+print("[tsne.test] done")
+
+print("[tsne.test] testing pairwise2t...")
+
+Qs = pairwise2t(t_pairwise)
+
+print("[tsne.test] done")
+
+print("[tsne.test] testing ptSNE...")
+
+encoder = nn.Sequential(
+    nn.Linear(784, 500),
+    nn.Sigmoid(),
+    nn.Linear(500, 500),
+    nn.Sigmoid(),
+    nn.Linear(500, 2000),
+    nn.Sigmoid(),
+    nn.Linear(2000, 2))
+
+ptsne = ptSNE(encoder)
+
+ptsne.train(xs, 40, 10, 0.1)
+
+print("[tsne.test] testing pairwise...")
 
 print("[tsne.test] all tests pass.")
--- tsne.py
+++ tsne.py
@@ -1,13 +1,15 @@
 """
-PyTorch implementation of t-SNE and paramatric t-SNE.
+PyTorch implementation of paramatric t-SNE.
 (C) Kai Xu
 University of Edinburgh, 2017
 """
 
 
 from __future__ import print_function
+
 import torch
-import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
 
 
 def preprocess_img(xs):
@@ -19,12 +21,96 @@ def preprocess_img(xs):
 
     return xs
 
-
-def pairwise(xs):
+def pairwise(xs, ppl=1):
 
     x_num, x_dim = xs.size()
     x1 = xs.unsqueeze(0).expand(x_num, x_num, x_dim)
     x2 = xs.unsqueeze(1).expand(x_num, x_num, x_dim)
-    dkl2 = ((x1 - x2) ** 2.0).sum(2).squeeze()
+    pws = ((x1 - x2) ** 2.0 / (2 * ppl)).sum(2)
 
-    return dkl2
+    return pws
+
+def pairwise2gauss(pws):
+
+    x_num, _ = pws.size()
+    pws_tmp = torch.exp(-pws)
+
+    pjgi = pws_tmp / pws_tmp.sum(0).view(x_num, 1).repeat(1, x_num)
+    pigj = pws_tmp / pws_tmp.sum(1).view(1, x_num).repeat(x_num, 1)
+    
+    pij = (pjgi + pigj) / (2 * x_num)
+
+    return pij
+
+def pairwise2t(pws, alpha=1):
+
+    pws_tmp = (1 + pws / alpha) ** (-(alpha + 1) / 2)
+
+    qij = pws_tmp / pws_tmp.sum()
+
+    return qij
+
+class ptSNE:
+
+    def __init__(self, encoder):
+
+        self.encoder = encoder
+
+    def train(self, xs, batch_size, epoch_num, lr):
+
+        x_num, x_dim = xs.size()
+
+        batches = map(lambda s: (s, s + batch_size - 1) if s + batch_size - 1 < x_num else x_num,
+        torch.arange(0, x_num, batch_size).int())
+        batch_num = len(batches)
+        log_skip = int(batch_num / 4)
+
+        Ps_l = []
+
+        for batch_s, batch_e in batches:
+            
+            xs_pairwise = pairwise(xs[batch_s:batch_e, :])
+            Ps_l.append(pairwise2gauss(xs_pairwise))
+
+        optimizer = optim.SGD(self.encoder.parameters(), lr=lr)
+        criterion = torch.nn.KLDivLoss(size_average=True)
+
+        log = dict()
+        log["cost"] = []
+
+        # self.encoder.init.normal()
+
+        for epoch in range(1, epoch_num + 1):
+
+            print("#epoch, #iter, cost")
+
+            running_cost = .0
+
+            for i, (batch_s, batch_e) in enumerate(batches, 1):
+
+                x = Variable(xs[batch_s:batch_e, :])
+                P = Variable(Ps_l[i - 1])
+
+                optimizer.zero_grad()
+
+                y = self.encoder(x)
+                y_pairwise = pairwise(y)
+
+                Q = pairwise2t(y_pairwise)
+
+                loss = criterion(Q, P)
+
+                running_cost += loss.data[0]
+                log["cost"].append(running_cost / i)
+
+                loss.backward()
+
+                optimizer.step()
+
+                if i % log_skip == 0:
+                    print("%6d, %5d, %.7f" %
+                          (epoch, i, running_cost / i))
+
+    def __call__(self, *args):
+
+        return self.encoder(*args)
